1780cdff11355d5e7918a750fbd9bd1354d89809
Libern <libernlin@gmail.com>
2016-09-27 21:16:38 +0800

    Improved Someline Image Test

--- tests/SomelineImageTest.php
+++ tests/SomelineImageTest.php
@@ -1,11 +1,29 @@
 <?php
 
+use Someline\Image\ImageTemplate;
+use Someline\Image\Models\SomelineImage;
+use Someline\Image\SomelineImageService;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class SomelineImageTest extends TestCase
 {
 
-    public function testSomelineImage()
+    public function testHandleImageUsingSomelineImageService()
     {
+        $file = new UploadedFile(base_path('/public/images/12745840728709.jpg'), '12745840728709.jpg', null, null, null, true);
 
+        $somelineImageService = new SomelineImageService();
+        $somelineImage = $somelineImageService->handleUploadedFile($file);
+        print_r($somelineImage->toArray());
+    }
+
+    public function testShowImage()
+    {
+        /** @var SomelineImage $somelineImage */
+        $somelineImage = SomelineImage::find(1);
+
+        $somelineImageService = new SomelineImageService();
+        $somelineImageService->showImage('original', $somelineImage->getImageName());
     }
 
 }
